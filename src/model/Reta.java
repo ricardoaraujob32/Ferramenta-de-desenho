edbaae0a46ca9b3a7b54378e7976449c0f9bb233
Ricardo de Araújo Balduino <ricardoaraujob32@gmail.com>
2018-12-26 22:06:46 -0200

    Inicializando
    Inicializando

---
+++ src/model/Reta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.awt.Point;
+/**
+ *
+ * @author ricardobalduino
+ */
+public class Reta {
+    private Point coord1;
+    private Point coord2;
+
+    public Point getCoord1() {
+        return coord1;
+    }
+
+    public Point getCoord2() {
+        return coord2;
+    }
+    
+    
+}
